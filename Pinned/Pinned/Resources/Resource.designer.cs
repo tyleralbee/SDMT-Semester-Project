47cb0866ed365f7c4aae0ad4ec4d5d49d6989585
Suveck <suveck@gmail.com>
2017-03-22 00:24:46 -0600

    added basic login screen w/ no backing

--- Pinned/Pinned/Resources/Resource.designer.cs
+++ Pinned/Pinned/Resources/Resource.designer.cs
@@ -44,8 +44,20 @@ namespace Pinned
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050004
+			public const int button1 = 2131034116;
+			
+			// aapt resource value: 0x7f050001
+			public const int editText1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int editText2 = 2131034114;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int imageView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050003
+			public const int myButton = 2131034115;
 			
 			static Id()
 			{
@@ -61,7 +73,10 @@ namespace Pinned
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int ListItem = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
