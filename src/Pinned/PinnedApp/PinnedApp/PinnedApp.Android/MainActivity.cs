80f6df612b049a906d96bd8485b57c4e4807ca90
CodyMattern <Cody.Mattern@colorado.edu>
2017-05-04 16:04:14 -0600

    Fixed Android, not being in new source dir.

--- src/Pinned/PinnedApp/PinnedApp/PinnedApp.Android/MainActivity.cs
+++ src/Pinned/PinnedApp/PinnedApp/PinnedApp.Android/MainActivity.cs
@@ -1,27 +1,36 @@
 ﻿using System;
 
 using Android.App;
+using Android.Content;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
 
-namespace PinnedApp.Droid
+namespace Pinned.Droid
 {
-    [Activity(Label = "PinnedApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
-        protected override void OnCreate(Bundle bundle)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
+	[Activity(Label = "Pinned.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
+            //MainResource = Resource.Layout.Main;
+            //LoginResource = Resource.Layout.Login;
 
             base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Login);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
+            Button button = FindViewById<Button>(Resource.Id.Login);
+
+
+
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+
+			LoadApplication(new App());
         }
-    }
+	}
 }
-
